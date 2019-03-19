e1c149edc8ba98d4782a8317904f5336dfaac42d
Thomas Kellner <thomas.kellner@stud.fh-campuswien.ac.at>
2019-03-19 16:28:42 +0100

    Judyta sagt kann gelöscht werden

--- CSS_LogIn/logout.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-  //LogOut the user by unsetting the sesison cookie
-  unset($_SESSION['login']);
-  header("Location: index.php");
-?>
-  
--- README.md
+++
@@ -1,2 +0,0 @@
-# RelaxDIY
-Semester work for WebTech
