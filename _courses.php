6d19a9008821899c984f2cb840731f519d55e63b
Elisabeth Kohlhofer <elisabeth.kohlhofer@stud.fh-campuswien.ac.at>
2019-05-13 23:25:48 +0200

    Kurseite verschönert! & verlinkt.

--- _courses.php
+++ _courses.php
@@ -4,81 +4,182 @@
 	include "PHP/footer.php";
 	include "PHP/navbar.php";
 ?>
+
 <!DOCTYPE html>
+
 <html>
 	<head>
 		<meta charset="UTF-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="CSS/team.css" type="text/css" rel="stylesheet">
 		<link href="CSS/general.css" type="text/css" rel="stylesheet">
-		
+
+        <script src="myScript.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	</head>
-<style>
 
- 
+    <style>
+    </style>
 
-</style>
 
 <body>
 
 	<header>
 	</header>
 
-	<main class="white-bgrnd" style="margin-bottom:70px;">
-		
-	<h1> Check out our courses… </h1>
+	<main class="white-bgrnd" style="overflow:hidden; margin-bottom:70px;">
 
-	<b> Origami with Anna </b>
-		<ul>
-			<li>Folding is fun – Helpful things to know before you get started</li>
-			<li>Tutorial – Crane</li>
-			<li>Fold your own Christmas decorations</li>
-		</ul>
+        <h1> Check out our courses… </h1>
 
-	<b> Calligraphy with Elisabeth </b>
-		<ul>
-			<li>Hand lettering: 4 easy steps to modern calligraphy </li>
-			<li>The ultimate guide to iPad lettering </li>
-			<li>How to find your own Calligraphy style </li> 
-		</ul>
 
-	<b> Cooking with Gaelle </b>
-		<ul>
-			<li>Peanut butter rocks!  </li>  
-			<li>My life with garlic – The perfect seasoning  </li> 
-			<li>13 ways to cook sweet potatoes  </li> 
-		</ul>
+    <!-------------------------------DESKTOP----------------------------------->
 
-	<b> Zumba with Judyta </b>
-		<ul> 
-			<li>Strong by Zumba  </li> 
-			<li>Zumba for nerds  </li> 
-			<li>Zumbini – Zumba for mommies & babies  </li> 
-		</ul>
-			
-	<b> Photography with Serena </b>
-		<ul>
-			<li>Your road to better photography </li> 
-			<li>How to make the most of Instagram filters </li> 
-			<li>Phone Photography - Because the best tool is the one you always have around </li> 
-		</ul>
-		
-	<b> Demons with Thomas </b>
-		<ul>
-			<li>Game of Demons </li> 
-			<li>How to kill a demon – The basics </li> 
-			<li>The road to exorcism – When is it really necessary? </li> 
-		</ul>
+    <div id="content-desktop" class="people">
+
+        <div>
+        <h3> Origami with Anna </h3>
+            <ul>
+                <li>Folding is fun – Helpful things to know before you get started</li></br>
+                <li>Tutorial – Crane</li></br>
+                <li>Fold your own Christmas decorations</li>
+            </ul>
+            <a id="goToid" class="goTo" href="origami.php""><b>Check out Anna's page!</b></a></br></br></br>
+        </div>
+
+        <div>
+            <h3> Calligraphy with Elisabeth </h3>
+            <ul>
+                <li>Hand lettering: 4 easy steps to modern calligraphy </li></br>
+                <li>The ultimate guide to iPad lettering </li></br>
+                <li>How to find your own calligraphy style </li>
+            </ul>
+            <a id="goToid" class="goTo" href="writing.php"><b>Check out Elisabeth's page!</b></a></br></br></br>
+        </div>
+
+        <!---------------------------------------------------------------------------------------------------------->
+
+        <div>
+            <h3> Cooking with Gaelle </h3>
+            <ul>
+                <li>Peanut butter rocks!  </li></br>
+                <li>My life with garlic – The perfect seasoning  </li></br>
+                <li>13 ways to cook sweet potatoes  </li>
+            </ul>
+            <a id="goToid" class="goTo" href="cooking.php"><b>Check out Gaelle's page!</b></a></br></br></br>
+        </div>
+
+        <div>
+            <h3> Zumba with Judyta </h3>
+            <ul>
+                <li>Strong by Zumba  </li></br>
+                <li>Zumba for nerds  </li></br>
+                <li>Zumbini – Zumba for mommies & babies  </li>
+            </ul>
+            <a id="goToid" class="goTo" href="_zumba.php"><b>Check out Judyta's page!</b></a></br></br></br>
+        </div>
+
+
+
+        <!---------------------------------------------------------------------------------------------------------->
+
+        <div>
+            <h3> Photography with Serena </h3>
+            <ul> <!-- The <td> tag defines a standard cell in an HTML table.-->
+                <li>Your road to better photography </li></br>
+                <li>How to make the most of Instagram filters </li></br>
+                <li>iPhone Photography - The best tool is the one you always have around </li>
+            </ul>
+            <a id="goToid" class="goTo" href="photography.php"><b>Check out Serena's page!</b></a></br></br></br>
+        </div>
+
+        <div>
+            <h3> Demons with Thomas  </h3>
+            <ul>
+                <li>Game of demons  </li></br>
+                <li>How to kill a demon – The basics  </li></br>
+                <li>The road to exorcism – When is it really necessary?  </li>
+            </ul>
+            <a id="goToid" class="goTo" href="demons.php"><b>Check out Thomas' page!</b></a></br></br></br>
+        </div>
+
+    </div>
+
+
+    <!---------------------------MOBILE---------------------------->
+
+
+    <div id="content-mobile" class="people" >
+
+        <div>
+            <h3> Origami with Anna </h3>
+            <ul>
+                <li>Folding is fun – Helpful things to know before you get started</li></br>
+                <li>Tutorial – Crane</li></br>
+                <li>Fold your own Christmas decorations</li>
+            </ul>
+            <a id="goToid" class="goTo" href="origami.php""><b>Check out Anna's page!</b></a></br></br></br>
+        </div>
+
+        <div>
+            <h3> Calligraphy with Elisabeth </h3>
+            <ul>
+                <li>Hand lettering: 4 easy steps to modern calligraphy </li></br>
+                <li>The ultimate guide to iPad lettering </li></br>
+                <li>How to find your own calligraphy style </li>
+            </ul>
+            <a id="goToid" class="goTo" href="writing.php"><b>Check out Elisabeth's page!</b></a></br></br></br>
+        </div>
+
+        <!---------------------------------------------------------------------------------------------------------->
+
+        <div>
+            <h3> Cooking with Gaelle </h3>
+            <ul>
+                <li>Peanut butter rocks!  </li></br>
+                <li>My life with garlic – The perfect seasoning  </li></br>
+                <li>13 ways to cook sweet potatoes  </li>
+            </ul>
+            <a id="goToid" class="goTo" href="cooking.php"><b>Check out Gaelle's page!</b></a></br></br></br>
+        </div>
+
+        <div>
+            <h3> Zumba with Judyta </h3>
+            <ul>
+                <li>Strong by Zumba  </li></br>
+                <li>Zumba for nerds  </li></br>
+                <li>Zumbini – Zumba for mommies & babies  </li>
+            </ul>
+            <a id="goToid" class="goTo" href="_zumba.php"><b>Check out Judyta's page!</b></a></br></br></br>
+        </div>
+
+    <!---------------------------------------------------------------------------------------------------------->
+
+        <div>
+            <h3> Photography with Serena </h3>
+            <ul>
+                <li>Your road to better photography </li></br>
+                <li>How to make the most of Instagram filters </li></br>
+                <li>iPhone Photography - The best tool is the one you always have around </li>
+            </ul>
+            <a id="goToid" class="goTo" href="photography.php"><b>Check out Serena's page!</b></a></br></br></br>
+        </div>
+
+        <div>
+            <h3> Demons with Thomas  </h3>
+            <ul>
+                <li>Game of demons  </li></br>
+                <li>How to kill a demon – The basics  </li></br>
+                <li>The road to exorcism – When is it really necessary?  </li>
+            </ul>
+            <a id="goToid" class="goTo" href="demons.php"><b>Check out Thomas' page!</b></a></br></br></br>
+        </div>
+
+    </div>
+
 		
 	</main>
 
 
-
-		<footer class="footer">
-			<strong>"<script src="http://numbersapi.com/random?write"></script>"</strong>
-			<br>
-			Copyright &copy; 2019 <a href="impressum.html">relaxDIY.com</a>
-		</footer>	
 </body>
 </html>
