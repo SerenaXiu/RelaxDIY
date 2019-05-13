9fbf22b70d22cb37b49a0cd0c3a33c8c202a55ef
Judyta Krzyzak <judyta.krzyzak@stud.fh-campuswien.ac.at>
2019-05-13 10:28:36 +0200

    BITTE pullen, ist mühsam, Kleinigkeiten mehrmals zu verbessern!

---
+++ partners.php
@@ -0,0 +1,35 @@
+<?php
+  session_start();
+	
+	include "PHP/footer.php";
+	include "PHP/navbar.php";
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link href="CSS/general.css" type="text/css" rel="stylesheet">
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>	
+	</head>
+
+
+<body id="generalBackground">
+
+	<header >
+	</header>
+
+	<main id="aboutText">  
+
+							<img src="Img/Coursespics/ptns.jpg" style ="width:70%; height: 70%;">
+							
+							
+						</a>
+	</main>
+
+
+	
+</body>
+</html>
